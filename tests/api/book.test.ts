43b3be1d2960d2201d25d7cc4a5b884228122c3b
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-06-30 00:21:13 -0400

    test POST /api/books

--- tests/api/book.test.ts
+++ tests/api/book.test.ts
@@ -6,8 +6,8 @@ import { Book } from '../../src/db/models/book.model';
 
 beforeAll(async () => {
     await sequelizeConnection.sync({force: true});
-    await Book.create({name: "vivo", isbn:"vivo123", lend: false});
-    await Book.create({name: "vivo1", isbn:"vivo12345", lend: false});
+    await Book.create({name: "Learn React", isbn:"305-598-21508-8", lend: false});
+    await Book.create({name: "The Truth", isbn:"443-598-21508-8", lend: false});
   });
   
   afterAll(async () => {
@@ -22,9 +22,26 @@ describe('GET /api/books', () => {
       .expect('Content-Type', /json/)
       .expect(200)
       .then((response) => {
-        expect(response.body).toHaveProperty('books');
-        expect(response.body.books).toHaveProperty('length');
-        expect(response.body.books.length).toEqual(2);
+        expect(response.body).toHaveProperty('length');
+        expect(response.body.length).toEqual(2);
       });
   });
+});
+
+describe('POST /api/books', () => {
+  it('responds with an inserted object', async () =>
+    request(app)
+      .post('/api/books')
+      .set('Accept', 'application/json')
+      .send({name: "Learn TypeScript", isbn:"3-598-21508-8", lend: false})
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('name');
+        expect(response.body.content).toBe('Learn TypeScript');
+        expect(response.body).toHaveProperty('isbn');
+        expect(response.body).toHaveProperty('lend');
+      }),
+  );
 });
